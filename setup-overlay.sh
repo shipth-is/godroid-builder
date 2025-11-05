5fa4d9ed437dfcb6ff67c887dee6f6a7e390e7cb
madebydavid <sutherland.dave@gmail.com>
2025-11-05 15:23:22 +0000

    base overlay for 4.1

--- setup-overlay.sh
+++ setup-overlay.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
+scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mkdir -p overlay/4.2
-cd overlay/4.2
+# Usage ./compare-overlay.sh <version>
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <godotVersion>"
+  exit 1
+fi
+godotVersion="$1"
+
+${scriptDir}/build-aar.sh "$godotVersion" --downloadOnly
+
+mkdir -p "$scriptDir/overlay/$godotVersion"
+cd "$scriptDir/overlay/$godotVersion"
 mkdir -p ./platform/android/java/lib/src/org/godotengine/godot/
 
-cp ../../godot-4.2/platform/android/file_access_android.h ./platform/android/file_access_android.h
-cp ../../godot-4.2/platform/android/detect.py ./platform/android/detect.py
-cp ../../godot-4.2/platform/android/file_access_android.cpp ./platform/android/file_access_android.cpp
-cp ../../godot-4.2/platform/android/SCsub ./platform/android/SCsub
-cp ../../godot-4.2/platform/android/java/lib/build.gradle ./platform/android/java/lib/build.gradle
-cp ../../godot-4.2/platform/android/java/lib/src/org/godotengine/godot/GodotFragment.java ./platform/android/java/lib/src/org/godotengine/godot/GodotFragment.java
-cp ../../godot-4.2/platform/android/java/lib/src/org/godotengine/godot/GodotLib.java ./platform/android/java/lib/src/org/godotengine/godot/GodotLib.java
-cp ../../godot-4.2/platform/android/java/lib/src/org/godotengine/godot/GodotDownloaderService.java ./platform/android/java/lib/src/org/godotengine/godot/GodotDownloaderService.java
-cp ../../godot-4.2/platform/android/java/lib/src/org/godotengine/godot/GodotDownloaderAlarmReceiver.java ./platform/android/java/lib/src/org/godotengine/godot/GodotDownloaderAlarmReceiver.java
-cp ../../godot-4.2/platform/android/java/lib/AndroidManifest.xml ./platform/android/java/lib/AndroidManifest.xml
+relativePath="../../godot-$godotVersion/"
+
+cp "$relativePath/platform/android/file_access_android.h" ./platform/android/file_access_android.h
+cp "$relativePath/platform/android/detect.py" ./platform/android/detect.py
+cp "$relativePath/platform/android/file_access_android.cpp" ./platform/android/file_access_android.cpp
+cp "$relativePath/platform/android/SCsub" ./platform/android/SCsub
+cp "$relativePath/platform/android/java/lib/build.gradle" ./platform/android/java/lib/build.gradle
+cp "$relativePath/platform/android/java/lib/src/org/godotengine/godot/GodotDownloaderService.java" ./platform/android/java/lib/src/org/godotengine/godot/GodotDownloaderService.java
+cp "$relativePath/platform/android/java/lib/src/org/godotengine/godot/GodotDownloaderAlarmReceiver.java" ./platform/android/java/lib/src/org/godotengine/godot/GodotDownloaderAlarmReceiver.java
+cp "$relativePath/platform/android/java/lib/AndroidManifest.xml" ./platform/android/java/lib/AndroidManifest.xml
+
+cp "$relativePath/platform/android/java/lib/src/org/godotengine/godot/GodotLib.java" ./platform/android/java/lib/src/org/godotengine/godot/GodotLib.java
+
+# one of these will be missing
+cp "$relativePath/platform/android/java/lib/src/org/godotengine/godot/Godot.java" ./platform/android/java/lib/src/org/godotengine/godot/Godot.java
+cp "$relativePath/platform/android/java/lib/src/org/godotengine/godot/GodotFragment.java" ./platform/android/java/lib/src/org/godotengine/godot/GodotFragment.java
 
 
